e7785bfed5e5fde28f2b96dace348f46799cff2e
Malik Bonnicksen <7426374+malikbonnicksen@users.noreply.github.com>
2024-11-19 12:31:16 +0100

    Add files via upload

---
+++ Encoder.py
@@ -0,0 +1,23 @@
+original = open("C:/Users/mab/OneDrive - HJ Hansen/Dokumenter/Tekst_to_encode.txt", 'r', encoding="utf8")
+lines = original.readlines()
+output = open("C:/Users/mab/OneDrive - HJ Hansen/Dokumenter/Encoded_text.txt", 'w', encoding="utf8")
+specials = {}
+with open("HTML special characters.txt", 'r', encoding="utf8") as f:
+    for line in f:
+        line = line.replace("=", "")
+        (key, val) = line.rstrip("\n").split()
+        specials[key] = val
+
+for line in lines:
+    number_part, text_part = line.split('\t')
+    words = text_part.split()
+    for i in range(len(words)):
+        for key in specials:
+            if key in words[i]:
+                words[i] = words[i].replace(key, specials[key])
+    updated_text_part = ' '.join(words)
+    updated_line = f"{number_part}\t{updated_text_part}"
+    output.write(updated_line + '\n')
+    
+
+
